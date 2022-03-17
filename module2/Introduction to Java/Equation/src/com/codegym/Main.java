de65bf985fb43c556104905e708bf81aa6b01b9a
unknown <you@example.com>
2022-03-17 15:57:07 +0700

    code

---
+++ module2/Introduction to Java/Equation/src/com/codegym/Main.java
@@ -0,0 +1,25 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Linear Equation Réolver");
+        System.out.println("Given a equation as 'a * x + b = c, please enter constants");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("a:");
+        double a = scanner.nextDouble();
+
+        System.out.println("b:");
+        double b = scanner.nextDouble();
+
+        System.out.println("c:");
+        double c = scanner.nextDouble();
+        if (a != 0) {
+            double answer = (c - b) / a;
+            System.out.println("equation pass with x =%f! \n");
+        } else {
+            System.out.println("no solution!");
+        }
+    }
+}
