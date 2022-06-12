96e07762cd7eee049e6f40304f2e206cfc45ba88
Huy123456789n <96730579+Huy123456789n@users.noreply.github.com>
2022-06-12 09:50:37 +0700

    Code

--- module2/OOP/src/Com/CodeGym/TestCode/Test.java
+++ module2/OOP/src/Com/CodeGym/TestCode/Test.java
@@ -1,20 +1,25 @@
 package Com.CodeGym.TestCode;
 
+import java.io.BufferedReader;
 import java.io.CharArrayReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class Test {
 
                 public static void main(String[] args) {
-                        int [] array = {3,4,2,6,4,2,8};
-                    int temp = array[0];
-                    for (int i = 1;i< array.length;i++) {
-                        if (temp == array[i]){
-                            System.out.println("số bị trùng" + array[i] + "temp " + temp);
-                        } else {
-                            temp = array[i+1];
-                        }
+                    String str = "Hello world";
+                    char temp = 0;
+                    char ch[]=str.toCharArray();
+                    for (int j =ch.length-1;j>=0;j--){
+                        System.out.print(ch[j]);
                     }
+//                    int [] array = {1,2,3,4,5,6,7,8};
+//                    int temp1 =0;
+//                 for (int i =array.length-1;i>=0;i--){
+//                     System.out.println(array[i]);
+//                 }
                 }
 }
